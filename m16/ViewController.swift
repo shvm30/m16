5ec7c58abc68b544ca4e33e48b0c6e0354da01de
shvm30 <shivilma18@icloud.com>
2023-02-20 18:40:59 +0300

    sfsdaf

---
+++ m16/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  m16
+//
+//  Created by Владимир on 20.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
